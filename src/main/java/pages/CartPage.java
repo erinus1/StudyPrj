36ff79a9624260eac1bbca139ae3b21e24c56cfd
sofiia.bondarenko <sofiiabondarenko91@gmail.com>
2023-05-11 14:21:27 +0300

    added structure of framework:PageObject

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,4 +1,14 @@
 package pages;
 
 public class CartPage {
+
+    public void reviewOrder() {
+        //перевірка кількості напоїв
+    }
+
+    public void clickContinue() {
+        //підтвердження і завершення замовення - join now, Sign In
+    }
+
+
 }
